5c80af5dabc749174d9d60ed7e7f2c0e9381db95
maryasay <91799033+maryasay@users.noreply.github.com>
2022-11-30 15:08:33 +0100

    Update web.py

--- web.py
+++ web.py
@@ -13,7 +13,7 @@ st.subheader("This is my to-do App")
 st.write("This app is to increase my productivity")
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo, key=todo[index])
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
